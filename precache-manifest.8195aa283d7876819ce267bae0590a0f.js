678b59ec63280672064bff5331cb4c8f8c178299
Vladimir Uzun <vlad.uzun@gmail.com>
2020-03-16 20:59:36 -0400

    Updates

---
+++ precache-manifest.8195aa283d7876819ce267bae0590a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddcfe7f4d2d1c3c6dab67459c0c5d89",
+    "url": "/bcbrowser/index.html"
+  },
+  {
+    "revision": "56035d85d5220d62c806",
+    "url": "/bcbrowser/static/css/main.bcd63513.chunk.css"
+  },
+  {
+    "revision": "7b57c5fcb95407946ea4",
+    "url": "/bcbrowser/static/js/2.2e69d7b6.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/bcbrowser/static/js/2.2e69d7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56035d85d5220d62c806",
+    "url": "/bcbrowser/static/js/main.f2a865e1.chunk.js"
+  },
+  {
+    "revision": "de8e00450fddf33e44b6",
+    "url": "/bcbrowser/static/js/runtime-main.1efde9f0.js"
+  }
+]);
